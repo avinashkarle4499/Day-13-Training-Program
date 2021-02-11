7b9e42965464561439f5424ed8986d6c9da61bf2
avinashkarle4499 <78337791+avinashkarle4499@users.noreply.github.com>
2021-02-11 11:44:18 +0530

    Update LinkedListDemo.java

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,27 +1,27 @@
 package day13;
-
+import java.util.LinkedList;
 import java.util.List;
 public class LinkedListDemo {
 
 	public static void main(String[] args) {
 
-		List 1 =new Linked list();
-		
+		LinkedList l = new LinkedList ();
+
 		l.add(2);
 		l.add(5);
 		l.addFirst(10);
 		l.addLast(4);
 		l.add(25);
 
-		System.out.println("Linked ist" + 1);
+		System.out.println("Linked list" + 1);
 
 		l.removeFirst();
 
-		System.out.println("Linked ist" + 1);
+		System.out.println("Linked list" + 1);
 
 		l.removeLast();
 
-		System.out.println("Linked ist" + 1);
+		System.out.println("Linked list" + 1);
 
 	}
 
